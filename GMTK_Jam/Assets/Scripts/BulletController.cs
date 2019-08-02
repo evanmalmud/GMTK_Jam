2d4557b46903f51247a672b66a32bbc583b344bc
TheRolf <yann.lv@live.fr>
2019-08-03 00:39:13 +0200

    tried some finished with bullet contolr bar

--- GMTK_Jam/Assets/Scripts/BulletController.cs
+++ GMTK_Jam/Assets/Scripts/BulletController.cs
@@ -5,15 +5,20 @@ public class BulletController : MonoBehaviour
 {
     public float InitialSpeed;
 
-    public float FocusMaxDuration = 2;
+    public float FocusMaxDuration;
+    private float FocusLeft;
 
     private Vector2 AxisSpeed;
+    private FocusTime ft;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ft = FindObjectOfType<FocusTime>();
+
+        FocusLeft = FocusMaxDuration;
     }
 
     public void SetAxisSpeed(Vector3 mousePosition) {
@@ -26,18 +31,26 @@ public class BulletController : MonoBehaviour
     {
         Vector2 sideSpeed = Vector2.zero;
 
-        if(!Input.GetKey(KeyCode.LeftArrow) || !Input.GetKey(KeyCode.RightArrow)) {
-            if(Input.GetKey(KeyCode.LeftArrow)) {
-                sideSpeed = Vector2.Perpendicular(AxisSpeed);
-            } else if(Input.GetKey(KeyCode.RightArrow)) {
-                sideSpeed = -Vector2.Perpendicular(AxisSpeed);
-            }
+        // if there is some focus left and a key is pressed
+        if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && FocusLeft > 0) {
+            sideSpeed = Vector2.Perpendicular(AxisSpeed);
+            
+            // to to opposite if right arrow
+            if(Input.GetKey(KeyCode.RightArrow))
+                sideSpeed *= -1;
 
-            if(!sideSpeed.Equals(Vector2.zero) && AxisSpeed.y < 0) {
-                sideSpeed = Vector2.Perpendicular(Vector2.Perpendicular(sideSpeed));
-            }
+            FocusLeft = Mathf.Max(0, FocusLeft - Time.deltaTime);
+            Debug.Log(FocusLeft);
+        }
+
+        // reverse when y axis down
+        if(!sideSpeed.Equals(Vector2.zero) && AxisSpeed.y < 0) {
+            sideSpeed = Vector2.Perpendicular(Vector2.Perpendicular(sideSpeed));
         }
         
+        // set speed
         rb.velocity = AxisSpeed + sideSpeed;
+        ft.UpdatePercent(FocusLeft / (FocusMaxDuration + 0.0000001f));
+        Debug.Log(FocusLeft);
     }
 }
