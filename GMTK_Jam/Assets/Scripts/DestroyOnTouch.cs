022e0def551f3fadbd2a89eb4451f7d8d40321d7
evanmalmud <evanmalmud@gmail.com>
2019-08-02 19:22:08 -0400

    PinBall Prototype
    Commiting Scene with another prototype.
    Implemented some generic scripts

---
+++ GMTK_Jam/Assets/Scripts/DestroyOnTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DestroyOnTouch : MonoBehaviour
+{
+
+    [TagSelector]
+    public string[] TagFilterArray = new string[] { };
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (string tag in TagFilterArray)
+        {
+            if (collision.gameObject.tag.Equals(tag))
+            {
+                // Destroy
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GMTK_Jam/Assets/Scripts/WASDMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class WASDMove : MonoBehaviour
+{
+    // Normal Movements Variables
+    public float moveSpeed;
+    public float spinSpeed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * moveSpeed, 0.8f), 
+            Mathf.Lerp(0, Input.GetAxis("Vertical") * moveSpeed, 0.8f));
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddTorque(spinSpeed);
+        }
+
+
+    }
+}
+
