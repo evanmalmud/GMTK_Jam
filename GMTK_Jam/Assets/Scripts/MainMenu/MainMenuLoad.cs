cfb21d622b3915dadc1c1611484c3dcabc7fffb4
evanmalmud <evanmalmud@gmail.com>
2019-08-02 22:00:39 -0400

    Added MainMenu and TestSceen
    Added a very simple MainMenu scene.
    Added a new BulletTest scene for a mouse based movement. Added some basic enemies and the bumpers.

---
+++ GMTK_Jam/Assets/Scripts/MainMenu/MainMenuLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoad : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public void loadMainScene()
+    {
+        SceneManager.LoadScene("BulletTest2", LoadSceneMode.Additive);
+    }
+}
--- GMTK_Jam/Assets/Scripts/MouseFollowMove.cs
+++ GMTK_Jam/Assets/Scripts/MouseFollowMove.cs
@@ -7,7 +7,12 @@ public class MouseFollowMove : MonoBehaviour
 {
     [SerializeField] float MoveSpeed = 350f;
     [SerializeField] float RotateSpeed = 2000f;
+    public int bounce;
     Rigidbody2D rb;
+
+    bool disableMovement = false;
+    float disableMovementTimer = 1.0f;
+    float disableMovementDefaultVal = 1.0f;
 
     // Use this for initialization
     void Start()
@@ -18,12 +23,31 @@ public class MouseFollowMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = transform.up * MoveSpeed * Time.deltaTime;
+        if(disableMovement)
+        {
+            disableMovementTimer -= Time.deltaTime;
+            if(disableMovementTimer <= 0)
+            {
+                disableMovement = false;
+            }
+        }
+        else
+        {
+            rb.velocity = transform.up * MoveSpeed * Time.deltaTime;
+            Vector3 targetVector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotatingIndex = Vector3.Cross(targetVector, transform.up).z;
+            rb.angularVelocity = -1 * rotatingIndex * RotateSpeed * Time.deltaTime;
+        }
+    }
 
-        Vector3 targetVector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Vector3 posDifference = transform.position - collision.transform.position;
+        //rb.AddForce(posDifference * bounce);
+        //disable movement
+        disableMovement = true;
 
-        float rotatingIndex = Vector3.Cross(targetVector, transform.up).z;
-
-        rb.angularVelocity = -1 * rotatingIndex * RotateSpeed * Time.deltaTime;
+        disableMovementTimer = disableMovementDefaultVal;
+        print("test");
     }
 }
