42383f34136beb134e962f12d466177e546c8539
TheRolf <yann.lv@live.fr>
2019-08-03 23:23:09 +0200

    Score

--- GMTK_Jam/Assets/Scripts/UI/ScoreTextPoints.cs
+++ GMTK_Jam/Assets/Scripts/UI/ScoreTextPoints.cs
@@ -8,13 +8,19 @@ using TMPro;
 public class ScoreTextPoints : MonoBehaviour
 {
     private TextMeshProUGUI text;
+
+    private static readonly int ScoreUpdate = Animator.StringToHash("ScoreUpdate");
+
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
+        text.text = "0";
     }
     
     public void UpdateScoreText(int score) {
         text.text = score.ToString();
+        if(GetComponent<Animator>())
+            GetComponent<Animator>().SetTrigger(ScoreUpdate);
     }
 }
