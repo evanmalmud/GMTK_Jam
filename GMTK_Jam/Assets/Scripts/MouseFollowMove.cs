54b654fcdda79d56a3a498f33e47c54ac97f8b09
evanmalmud <evanmalmud@gmail.com>
2019-08-03 09:57:20 -0400

    Updates to Ball Prefab
    Moving get child call to inspector link

--- GMTK_Jam/Assets/Scripts/MouseFollowMove.cs
+++ GMTK_Jam/Assets/Scripts/MouseFollowMove.cs
@@ -7,21 +7,19 @@ public class MouseFollowMove : MonoBehaviour
 {
     [SerializeField] float MoveSpeed = 350f;
     [SerializeField] float RotateSpeed = 2000f;
-    
-    GameObject Diamond;
 
-    public int bounce;
+    [SerializeField] GameObject Diamond;
+
     Rigidbody2D rb;
 
     bool disableMovement = false;
     float disableMovementTimer = 1.0f;
-    float disableMovementDefaultVal = 1.0f;
+    [SerializeField] float disableMovementDefaultVal = 1.0f;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Diamond = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -47,13 +45,9 @@ public class MouseFollowMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Vector3 posDifference = transform.position - collision.transform.position;
-        //rb.AddForce(posDifference * bounce);
         //disable movement
         disableMovement = true;
         Diamond.SetActive(false);
-
         disableMovementTimer = disableMovementDefaultVal;
-        print("test");
     }
 }
