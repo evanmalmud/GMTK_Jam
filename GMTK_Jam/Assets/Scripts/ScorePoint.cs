71928c36e7d092ea5f2fa0ad6ead58d13a244ee5
TheRolf <yann.lv@live.fr>
2019-08-03 14:22:53 +0200

    hit infos

---
+++ GMTK_Jam/Assets/Scripts/ScorePoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScorePoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SetTextAndDestroy(int points) {
+        GetComponent<TextMeshPro>().text = points.ToString();
+
+        StartCoroutine("WaitAndDestroy");
+    }
+    
+    IEnumerator WaitAndDestroy() {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
