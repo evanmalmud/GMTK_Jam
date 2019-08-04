acabe3edfbf828e553e6929c71867699e9b81fc8
evanmalmud <evanmalmud@gmail.com>
2019-08-04 09:05:43 -0400

    GamePlay Updates per Syranos added a Player Spawner (BulletManager)

---
+++ GMTK_Jam/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public GameObject playerToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    public void OnPlayerDestroy()
+    {
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        //This does not destroy the old one
+        GameObject go = Instantiate(playerToSpawn,this.transform);
+        go.SetActive(true);
+    }
+}
